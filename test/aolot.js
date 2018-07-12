b8e94c8396b541dc141fea9b89db1d10deaf6025
Johan <johan@paramation.com>
2018-07-12 20:13:10 +0700

    Add numLotsByAddress(), lotOfOwnerByIndex(), lotById() and also write tests for those functions

--- test/aolot.js
+++ test/aolot.js
@@ -15,19 +15,18 @@ contract("AOLot", function(accounts) {
 	});
 
 	it("should set AO Token Address", function() {
-		return lotMeta.AOTokenAddress.call()
-			.then(function(address) {
-				assert.notEqual(address, "0x0", "Contract does not set AO Token Address");
-			});
+		return lotMeta.AOTokenAddress.call().then(function(address) {
+			assert.notEqual(address, "0x0", "Contract does not set AO Token Address");
+		});
 	});
 	it("should have buy price at 10000 Wei/AO", function() {
-		return lotMeta.buyPrice.call()
-			.then(function(buyPrice) {
-				assert.equal(buyPrice.toNumber(), 10000, "Buy price is not set correctly");
-			});
+		return lotMeta.buyPrice.call().then(function(buyPrice) {
+			assert.equal(buyPrice.toNumber(), 10000, "Buy price is not set correctly");
+		});
 	});
 	it("can pause contract", function() {
-		return lotMeta.setPaused(true, { from: accounts[0] })
+		return lotMeta
+			.setPaused(true, { from: accounts[0] })
 			.then(function() {
 				return lotMeta.paused.call();
 			})
@@ -36,7 +35,8 @@ contract("AOLot", function(accounts) {
 			});
 	});
 	it("can unpause contract", function() {
-		return lotMeta.setPaused(false, { from: accounts[0] })
+		return lotMeta
+			.setPaused(false, { from: accounts[0] })
 			.then(function() {
 				return lotMeta.paused.call();
 			})
@@ -45,7 +45,8 @@ contract("AOLot", function(accounts) {
 			});
 	});
 	it("can set buy price", function() {
-		return lotMeta.setBuyPrice(10000, { from: accounts[0] })
+		return lotMeta
+			.setBuyPrice(10000, { from: accounts[0] })
 			.then(function() {
 				return lotMeta.buyPrice.call();
 			})
@@ -54,7 +55,8 @@ contract("AOLot", function(accounts) {
 			});
 	});
 	it("should have 1 Peta AO", function() {
-		return tokenMeta.mintToken(lotMeta.address, web3.toWei(10 ** 15, "ether"))
+		return tokenMeta
+			.mintToken(lotMeta.address, web3.toWei(10 ** 15, "ether"))
 			.then(function() {
 				return tokenMeta.balanceOf.call(lotMeta.address);
 			})
@@ -64,7 +66,8 @@ contract("AOLot", function(accounts) {
 	});
 	it("should send 20 AO to investor", function() {
 		var totalTokenBought;
-		return lotMeta.buy({ from: accounts[1], value: 200000 })
+		return lotMeta
+			.buy({ from: accounts[1], value: 200000 })
 			.then(function() {
 				return lotMeta.totalTokenBought.call();
 			})
@@ -78,22 +81,19 @@ contract("AOLot", function(accounts) {
 			});
 	});
 	it("should return the correct number of lots owned by an address", function() {
-		return lotMeta.numLotsByAddress(accounts[1])
-			.then(function(numLots) {
-				assert.equal(numLots.toNumber(), 1, "Returns wrong number of lots owned by an address");
-			});
+		return lotMeta.numLotsByAddress(accounts[1]).then(function(numLots) {
+			assert.equal(numLots.toNumber(), 1, "Returns wrong number of lots owned by an address");
+		});
 	});
 	it("should return the correct lot ID at a given index of owner's lots list", function() {
-		return lotMeta.lotOfOwnerByIndex(accounts[1], 0)
-			.then(function(numLots) {
-				assert.equal(numLots.toNumber(), 1, "Returns wrong lot ID at a given index");
-			});
+		return lotMeta.lotOfOwnerByIndex(accounts[1], 0).then(function(numLots) {
+			assert.equal(numLots.toNumber(), 1, "Returns wrong lot ID at a given index");
+		});
 	});
 	it("should return the correct lot at a given lot ID", function() {
-		return lotMeta.lotById(1)
-			.then(function(lot) {
-				assert.equal(lot[0], accounts[1], "Returns wrong address of the lot");
-				assert.equal(lot[1].toNumber(), web3.toWei(20, 'ether'), "Returns wrong tokenAmount of the lot");
-			});
+		return lotMeta.lotById(1).then(function(lot) {
+			assert.equal(lot[0], accounts[1], "Returns wrong address of the lot");
+			assert.equal(lot[1].toNumber(), web3.toWei(20, "ether"), "Returns wrong tokenAmount of the lot");
+		});
 	});
 });
