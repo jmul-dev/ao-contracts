61107adfb1e39779332fccd95ca75df72d64ab84
Johan <johan@paramation.com>
2018-10-29 15:23:12 +0700

    Add add pending setting

--- contracts/AOSetting.sol
+++ contracts/AOSetting.sol
@@ -29,14 +29,14 @@ contract AOSetting {
 		 * 5 => string (catch all)
 		 */
 		uint8 settingType;
+		bool pendingCreate;				// State when associatedThoughtId has not accepted setting
+		bool locked;					// State when pending anything (cannot change if locked)
 		string settingDataJSON;			// Catch-all
 	}
 
 	struct SettingState {
 		uint256 settingId;				// Identifier of this setting
-		bool pendingAdvocateCreate;		// State when associatedThoughtId has not accepted setting
-		bool pendingUpdateThought;		// State when setting is in process of being updated
-		bool locked;					// State when pending anything (cannot change if locked)
+		bool pendingUpdate;				// State when setting is in process of being updated
 		address updateAdvocateNameId;	// The nameId of the Advocate that performed the update
 
 		/**
@@ -49,7 +49,7 @@ contract AOSetting {
 
 		/**
 		 * Signature of the proposalThoughtId and update value by the associatedThoughtId
-		 * Advocate's Name's Address.
+		 * Advocate's Name's address.
 		 */
 		string updateSignature;
 
@@ -159,11 +159,248 @@ contract AOSetting {
 	// Mapping from updateHashKey to it's settingId
 	mapping (bytes32 => uint256) public updateHashLookup;
 
+	// Event to be broadcasted to public when pending setting is created
+	event PendingSettingCreation(uint256 indexed settingId, address indexed creatorNameId, address creatorThoughtId, address associatedThoughtId, string settingName, uint8 settingType, bytes32 associatedThoughtSettingId, bytes32 creatorThoughtSettingId);
+
+	// Event to be broadcasted to public when pending setting is approved
+	event ApprovePendingSettingCreation(uint256 indexed settingId, address associatedThoughtAdvocate);
+
 	/**
 	 * @dev Constructor function
 	 */
 	constructor(address _nameFactoryAddress) public {
 		nameFactoryAddress = _nameFactoryAddress;
 		_nameFactory = NameFactory(nameFactoryAddress);
+	}
+
+	/**
+	 * @dev Check if `_thoughtId` is a Thought
+	 */
+	modifier isThought(address _thoughtId) {
+		require (Thought(_thoughtId).originNameId() != address(0) && Thought(_thoughtId).thoughtTypeId() == 0);
+		_;
+	}
+
+	/**
+	 * @dev Check if `_settingName` of `_associatedThoughtId` is taken
+	 */
+	modifier settingNameNotTaken(string _settingName, address _associatedThoughtId) {
+		require (isSettingNameTaken(_settingName, _associatedThoughtId) == false);
+		_;
+	}
+
+	/***** Public Methods *****/
+	/**
+	 * @dev Check whether or not a setting name of an associatedThoughtId is taken
+	 * @param _settingName The human-readable name of the setting
+	 * @param _associatedThoughtId The thoughtId that the setting affects
+	 * @return true if taken. false otherwise
+	 */
+	function isSettingNameTaken(string _settingName, address _associatedThoughtId) public view returns (bool) {
+		return (nameSettingLookup[_associatedThoughtId][keccak256(abi.encodePacked(this, _settingName))] == 0);
+	}
+
+	/**
+	 * @dev Advocate of _creatorThoughtId adds a uint setting
+	 * @param _settingName The human-readable name of the setting
+	 * @param _value The uint256 value of the setting
+	 * @param _creatorThoughtId The thoughtId that created the setting
+	 * @param _associatedThoughtId The thoughtId that the setting affects
+	 * @param _extraData Catch-all string value to be stored if exist
+	 */
+	function addUintSetting(string _settingName, uint256 _value, address _creatorThoughtId, address _associatedThoughtId, string _extraData) public isThought(_creatorThoughtId) isThought(_associatedThoughtId) settingNameNotTaken(_settingName, _associatedThoughtId) {
+		// Make sure msg.sender is the Advocate of _creatorThoughtId
+		address _creatorNameId = _nameFactory.ethAddressToNameId(msg.sender);
+		require (_creatorNameId != 0 && _creatorNameId == Thought(_creatorThoughtId).advocateId());
+
+		// Update global variables
+		totalSetting++;
+
+		// Store the value as pending value
+		pendingSettingUintValue[totalSetting] = _value;
+
+		// Store pending setting creation data
+		_storePendingSettingCreation(_creatorNameId, 1, _settingName, _creatorThoughtId, _associatedThoughtId, _extraData);
+	}
+
+	/**
+	 * @dev Advocate of _creatorThoughtId adds a bool setting
+	 * @param _settingName The human-readable name of the setting
+	 * @param _value The bool value of the setting
+	 * @param _creatorThoughtId The thoughtId that created the setting
+	 * @param _associatedThoughtId The thoughtId that the setting affects
+	 * @param _extraData Catch-all string value to be stored if exist
+	 */
+	function addBoolSetting(string _settingName, bool _value, address _creatorThoughtId, address _associatedThoughtId, string _extraData) public isThought(_creatorThoughtId) isThought(_associatedThoughtId) settingNameNotTaken(_settingName, _associatedThoughtId) {
+		// Make sure msg.sender is the Advocate of _creatorThoughtId
+		address _creatorNameId = _nameFactory.ethAddressToNameId(msg.sender);
+		require (_creatorNameId != 0 && _creatorNameId == Thought(_creatorThoughtId).advocateId());
+
+		// Update global variables
+		totalSetting++;
+
+		// Store the value as pending value
+		pendingSettingBoolValue[totalSetting] = _value;
+
+		// Store pending setting creation data
+		_storePendingSettingCreation(_creatorNameId, 2, _settingName, _creatorThoughtId, _associatedThoughtId, _extraData);
+	}
+
+	/**
+	 * @dev Advocate of _creatorThoughtId adds an address setting
+	 * @param _settingName The human-readable name of the setting
+	 * @param _value The address value of the setting
+	 * @param _creatorThoughtId The thoughtId that created the setting
+	 * @param _associatedThoughtId The thoughtId that the setting affects
+	 * @param _extraData Catch-all string value to be stored if exist
+	 */
+	function addAddressSetting(string _settingName, address _value, address _creatorThoughtId, address _associatedThoughtId, string _extraData) public isThought(_creatorThoughtId) isThought(_associatedThoughtId) settingNameNotTaken(_settingName, _associatedThoughtId) {
+		// Make sure msg.sender is the Advocate of _creatorThoughtId
+		address _creatorNameId = _nameFactory.ethAddressToNameId(msg.sender);
+		require (_creatorNameId != 0 && _creatorNameId == Thought(_creatorThoughtId).advocateId());
+
+		// Update global variables
+		totalSetting++;
+
+		// Store the value as pending value
+		pendingSettingAddressValue[totalSetting] = _value;
+
+		// Store pending setting creation data
+		_storePendingSettingCreation(_creatorNameId, 3, _settingName, _creatorThoughtId, _associatedThoughtId, _extraData);
+	}
+
+	/**
+	 * @dev Advocate of _creatorThoughtId adds a bytes32 setting
+	 * @param _settingName The human-readable name of the setting
+	 * @param _value The bytes32 value of the setting
+	 * @param _creatorThoughtId The thoughtId that created the setting
+	 * @param _associatedThoughtId The thoughtId that the setting affects
+	 * @param _extraData Catch-all string value to be stored if exist
+	 */
+	function addBytesSetting(string _settingName, bytes32 _value, address _creatorThoughtId, address _associatedThoughtId, string _extraData) public isThought(_creatorThoughtId) isThought(_associatedThoughtId) settingNameNotTaken(_settingName, _associatedThoughtId) {
+		// Make sure msg.sender is the Advocate of _creatorThoughtId
+		address _creatorNameId = _nameFactory.ethAddressToNameId(msg.sender);
+		require (_creatorNameId != 0 && _creatorNameId == Thought(_creatorThoughtId).advocateId());
+
+		// Update global variables
+		totalSetting++;
+
+		// Store the value as pending value
+		pendingSettingBytesValue[totalSetting] = _value;
+
+		// Store pending setting creation data
+		_storePendingSettingCreation(_creatorNameId, 4, _settingName, _creatorThoughtId, _associatedThoughtId, _extraData);
+	}
+
+	/**
+	 * @dev Advocate of _creatorThoughtId adds a string setting
+	 * @param _settingName The human-readable name of the setting
+	 * @param _value The string value of the setting
+	 * @param _creatorThoughtId The thoughtId that created the setting
+	 * @param _associatedThoughtId The thoughtId that the setting affects
+	 * @param _extraData Catch-all string value to be stored if exist
+	 */
+	function addStringSetting(string _settingName, string _value, address _creatorThoughtId, address _associatedThoughtId, string _extraData) public isThought(_creatorThoughtId) isThought(_associatedThoughtId) settingNameNotTaken(_settingName, _associatedThoughtId) {
+		// Make sure msg.sender is the Advocate of _creatorThoughtId
+		address _creatorNameId = _nameFactory.ethAddressToNameId(msg.sender);
+		require (_creatorNameId != 0 && _creatorNameId == Thought(_creatorThoughtId).advocateId());
+
+		// Update global variables
+		totalSetting++;
+
+		// Store the value as pending value
+		pendingSettingStringValue[totalSetting] = _value;
+
+		// Store pending setting creation data
+		_storePendingSettingCreation(_creatorNameId, 5, _settingName, _creatorThoughtId, _associatedThoughtId, _extraData);
+	}
+
+	/**
+	 * @dev Advocate of SettingData's _associatedThoughtId approves pending setting creation
+	 * @param _settingId The ID of the setting to approve
+	 */
+	function approvePendingSettingCreation(uint256 _settingId) public {
+		// Make sure setting exists and needs approval
+		SettingData storage _settingData = settingDatas[_settingId];
+		address _associatedThoughtAdvocate = _nameFactory.ethAddressToNameId(msg.sender);
+		require (_settingData.settingId == _settingId && _settingData.pendingCreate == true && _associatedThoughtAdvocate != address(0) && _associatedThoughtAdvocate == Thought(_settingData.associatedThoughtId).advocateId());
+
+		// Mark setting as approved
+		_settingData.pendingCreate = false;
+
+		// If settingType == uint256
+		if (_settingData.settingType == 1) {
+			uint256 _uintValue = pendingSettingUintValue[_settingData.settingId];
+			pendingSettingUintValue[_settingData.settingId] = 0;
+			settingUintValue[_settingData.settingId] = _uintValue;
+		} else if (_settingData.settingType == 2) {
+			// Else if settingType == bool
+			bool _boolValue = pendingSettingBoolValue[_settingData.settingId];
+			pendingSettingBoolValue[_settingData.settingId] = false;
+			settingBoolValue[_settingData.settingId] = _boolValue;
+		} else if (_settingData.settingType == 3) {
+			// Else if settingType == address
+			address _addressValue = pendingSettingAddressValue[_settingData.settingId];
+			pendingSettingAddressValue[_settingData.settingId] = address(0);
+			settingAddressValue[_settingData.settingId] = _addressValue;
+		} else if (_settingData.settingType == 4) {
+			// Else if settingType == bytes32
+			bytes32 _bytesValue = pendingSettingBytesValue[_settingData.settingId];
+			pendingSettingBytesValue[_settingData.settingId] = '';
+			settingBytesValue[_settingData.settingId] = _bytesValue;
+		} else {
+			// Else if settingType == string
+			string memory _stringValue = pendingSettingStringValue[_settingData.settingId];
+			pendingSettingStringValue[_settingData.settingId] = '';
+			settingStringValue[_settingData.settingId] = _stringValue;
+		}
+
+		emit ApprovePendingSettingCreation(_settingData.settingId, _associatedThoughtAdvocate);
+	}
+
+	/***** Internal Method *****/
+	/**
+	 * @dev Store the pending setting creation data
+	 * @param _creatorNameId The nameId that created the setting
+	 * @param _settingType The type of this setting. 1 => uint256, 2 => bool, 3 => address, 4 => bytes32, 5 => string
+	 * @param _settingName The human-readable name of the setting
+	 * @param _creatorThoughtId The thoughtId that created the setting
+	 * @param _associatedThoughtId The thoughtId that the setting affects
+	 * @param _extraData Catch-all string value to be stored if exist
+	 */
+	function _storePendingSettingCreation(address _creatorNameId, uint8 _settingType, string _settingName, address _creatorThoughtId, address _associatedThoughtId, string _extraData) internal {
+		// Make sure _settingType is in supported list
+		require (_settingType >= 1 && _settingType <= 5);
+
+		// Update nameSettingLookup
+		nameSettingLookup[_associatedThoughtId][keccak256(abi.encodePacked(this, _settingName))] = totalSetting;
+
+		// Store setting data
+		SettingData storage _settingData = settingDatas[totalSetting];
+		_settingData.settingId = totalSetting;
+		_settingData.creatorNameId = _creatorNameId;
+		_settingData.creatorThoughtId = _creatorThoughtId;
+		_settingData.associatedThoughtId = _associatedThoughtId;
+		_settingData.settingName = _settingName;
+		_settingData.settingType = _settingType;
+		_settingData.pendingCreate = true;
+		_settingData.locked = true;
+		_settingData.settingDataJSON = _extraData;
+
+		// Store the associatedThoughtSetting info
+		bytes32 _associatedThoughtSettingId = keccak256(abi.encodePacked(this, _associatedThoughtId, totalSetting));
+		AssociatedThoughtSetting storage _associatedThoughtSetting = associatedThoughtSettings[_associatedThoughtSettingId];
+		_associatedThoughtSetting.associatedThoughtSettingId = _associatedThoughtSettingId;
+		_associatedThoughtSetting.associatedThoughtId = _associatedThoughtId;
+		_associatedThoughtSetting.settingId = totalSetting;
+
+		// Store the creatorThoughtSetting info
+		bytes32 _creatorThoughtSettingId = keccak256(abi.encodePacked(this, _creatorThoughtId, totalSetting));
+		CreatorThoughtSetting storage _creatorThoughtSetting = creatorThoughtSettings[_creatorThoughtSettingId];
+		_creatorThoughtSetting.creatorThoughtSettingId = _creatorThoughtSettingId;
+		_creatorThoughtSetting.creatorThoughtId = _creatorThoughtId;
+		_creatorThoughtSetting.settingId = totalSetting;
+
+		emit PendingSettingCreation(_settingData.settingId, _settingData.creatorNameId, _settingData.creatorThoughtId, _settingData.associatedThoughtId, _settingData.settingName, _settingData.settingType, _associatedThoughtSettingId, _creatorThoughtSettingId);
 	}
 }
