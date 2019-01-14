292300ce39574feb71eed5cb797b24a7286c2119
Johan <johan@paramation.com>
2019-01-14 12:49:12 +0700

    Add testing for pathos

---
+++ test/pathospeta.js
@@ -0,0 +1,337 @@
+var NameFactory = artifacts.require("./NameFactory.sol");
+var TAOFactory = artifacts.require("./TAOFactory.sol");
+var NameTAOPosition = artifacts.require("./NameTAOPosition.sol");
+var PathosPeta = artifacts.require("./PathosPeta.sol");
+var Logos = artifacts.require("./Logos.sol");
+
+contract("Pathos Peta", function(accounts) {
+	var namefactory, taofactory, nametaoposition, pathospeta, logos, nameId1, nameId2, taoId1, taoId2;
+	var theAO = accounts[0];
+	var account1 = accounts[1];
+	var account2 = accounts[2];
+	var someAddress = accounts[3];
+	var whitelistedAddress = accounts[4];
+
+	before(async function() {
+		namefactory = await NameFactory.deployed();
+		taofactory = await TAOFactory.deployed();
+		nametaoposition = await NameTAOPosition.deployed();
+		pathospeta = await PathosPeta.deployed();
+		logos = await Logos.deployed();
+
+		// Create Name
+		var result = await namefactory.createName("charlie", "somedathash", "somedatabase", "somekeyvalue", "somecontentid", {
+			from: account1
+		});
+		nameId1 = await namefactory.ethAddressToNameId(account1);
+
+		var result = await namefactory.createName("delta", "somedathash", "somedatabase", "somekeyvalue", "somecontentid", {
+			from: account2
+		});
+		nameId2 = await namefactory.ethAddressToNameId(account2);
+
+		// Mint Logos to nameId1 and nameId2
+		await logos.setWhitelist(theAO, true, { from: theAO });
+		await logos.mintToken(nameId1, 10 ** 12, { from: theAO });
+		await logos.mintToken(nameId2, 10 ** 12, { from: theAO });
+
+		result = await taofactory.createTAO(
+			"Charlie's TAO",
+			"somedathash",
+			"somedatabase",
+			"somekeyvalue",
+			"somecontentid",
+			nameId1,
+			0,
+			false,
+			0,
+			{
+				from: account1
+			}
+		);
+		var createTAOEvent = result.logs[0];
+		taoId1 = createTAOEvent.args.taoId;
+
+		result = await taofactory.createTAO(
+			"Delta's TAO",
+			"somedathash",
+			"somedatabase",
+			"somekeyvalue",
+			"somecontentid",
+			nameId2,
+			0,
+			false,
+			0,
+			{
+				from: account2
+			}
+		);
+		var createTAOEvent = result.logs[0];
+		taoId2 = createTAOEvent.args.taoId;
+	});
+
+	it("should have the correct totalSupply", async function() {
+		var totalSupply = await pathospeta.totalSupply();
+		assert.equal(totalSupply.toNumber(), 0, "Contract has incorrect totalSupply");
+	});
+
+	it("should have the correct name", async function() {
+		var name = await pathospeta.name();
+		assert.equal(name, "Pathos Peta", "Contract has incorrect name");
+	});
+
+	it("should have the correct symbol", async function() {
+		var symbol = await pathospeta.symbol();
+		assert.equal(symbol, "PATHOSPETA", "Contract has incorrect symbol");
+	});
+
+	it("should have the correct powerOfTen", async function() {
+		var powerOfTen = await pathospeta.powerOfTen();
+		assert.equal(powerOfTen.toNumber(), 15, "Contract has incorrect powerOfTen");
+	});
+
+	it("should have the correct decimals", async function() {
+		var decimals = await pathospeta.decimals();
+		assert.equal(decimals.toNumber(), 15, "Contract has incorrect decimals");
+	});
+
+	it("should have the correct nameTAOPositionAddress", async function() {
+		var nameTAOPositionAddress = await pathospeta.nameTAOPositionAddress();
+		assert.equal(nameTAOPositionAddress, nametaoposition.address, "Contract has incorrect nameTAOPositionAddress");
+	});
+
+	it("The AO - should be able to transfer ownership to a TAO", async function() {
+		var canTransferOwnership;
+		try {
+			await pathospeta.transferOwnership(taoId1, { from: someAddress });
+			canTransferOwnership = true;
+		} catch (e) {
+			canTransferOwnership = false;
+		}
+		assert.equal(canTransferOwnership, false, "Non-AO can transfer ownership");
+
+		try {
+			await pathospeta.transferOwnership(taoId1, { from: theAO });
+			canTransferOwnership = true;
+		} catch (e) {
+			canTransferOwnership = false;
+		}
+		assert.equal(canTransferOwnership, true, "The AO can't transfer ownership");
+
+		var newTheAO = await pathospeta.theAO();
+		assert.equal(newTheAO, taoId1, "Contract has incorrect TheAO address after transferring ownership");
+	});
+
+	it("The AO - should be able to whitelist an address", async function() {
+		var canSetWhitelist;
+		try {
+			await pathospeta.setWhitelist(whitelistedAddress, true, { from: someAddress });
+			canSetWhitelist = true;
+		} catch (e) {
+			canSetWhitelist = false;
+		}
+		assert.equal(canSetWhitelist, false, "Non-AO can set whitelist");
+
+		try {
+			await pathospeta.setWhitelist(whitelistedAddress, true, { from: account1 });
+			canSetWhitelist = true;
+		} catch (e) {
+			canSetWhitelist = false;
+		}
+		assert.equal(canSetWhitelist, true, "The AO can't set whitelist");
+
+		var whitelistStatus = await pathospeta.whitelist(whitelistedAddress);
+		assert.equal(whitelistStatus, true, "Contract returns incorrect whitelist status for an address");
+	});
+
+	it("The AO - should be able to set NameTAOPosition address", async function() {
+		var canSetAddress;
+		try {
+			await pathospeta.setNameTAOPositionAddress(nametaoposition.address, { from: someAddress });
+			canSetAddress = true;
+		} catch (e) {
+			canSetAddress = false;
+		}
+		assert.equal(canSetAddress, false, "Non-AO can set NameTAOPosition address");
+
+		try {
+			await pathospeta.setNameTAOPositionAddress(nametaoposition.address, { from: account1 });
+			canSetAddress = true;
+		} catch (e) {
+			canSetAddress = false;
+		}
+		assert.equal(canSetAddress, true, "The AO can't set NameTAOPosition address");
+
+		var nameTAOPositionAddress = await pathospeta.nameTAOPositionAddress();
+		assert.equal(nameTAOPositionAddress, nametaoposition.address, "Contract has incorrect nameTAOPositionAddress");
+	});
+
+	it("Whitelisted Address - should be able to mint token to a TAO/Name", async function() {
+		var canMintToken;
+		try {
+			await pathospeta.mintToken(nameId1, 1000, { from: someAddress });
+			canMintToken = true;
+		} catch (e) {
+			canMintToken = false;
+		}
+		assert.equal(canMintToken, false, "Non-whitelisted address can mint token");
+
+		try {
+			await pathospeta.mintToken(someAddress, 1000, { from: whitelistedAddress });
+			canMintToken = true;
+		} catch (e) {
+			canMintToken = false;
+		}
+		assert.equal(canMintToken, false, "Whitelisted address can mint token to non Name/TAO");
+
+		try {
+			await pathospeta.mintToken(nameId1, 1000, { from: whitelistedAddress });
+			canMintToken = true;
+		} catch (e) {
+			canMintToken = false;
+		}
+		assert.equal(canMintToken, true, "Whitelisted address can't mint token to Name");
+
+		var nameBalance = await pathospeta.balanceOf(nameId1);
+		assert.equal(nameBalance.toNumber(), 1000, "Name has incorrect pathospeta balance");
+
+		try {
+			await pathospeta.mintToken(taoId1, 1000, { from: whitelistedAddress });
+			canMintToken = true;
+		} catch (e) {
+			canMintToken = false;
+		}
+		assert.equal(canMintToken, true, "Whitelisted address can't mint token to TAO");
+
+		var taoBalance = await pathospeta.balanceOf(nameId1);
+		assert.equal(taoBalance.toNumber(), 1000, "TAO has incorrect pathospeta balance");
+	});
+
+	it("Whitelisted Address - should be able to transfer token from a Name/TAO to another Name/TAO", async function() {
+		var canTransferFrom;
+		try {
+			await pathospeta.transferFrom(nameId1, nameId2, 10, { from: someAddress });
+			canTransferFrom = true;
+		} catch (e) {
+			canTransferFrom = false;
+		}
+		assert.equal(canTransferFrom, false, "Non-whitelisted address can transfer token");
+
+		try {
+			await pathospeta.transferFrom(nameId1, someAddress, 10, { from: whitelistedAddress });
+			canTransferFrom = true;
+		} catch (e) {
+			canTransferFrom = false;
+		}
+		assert.equal(canTransferFrom, false, "Whitelisted address can transfer token to non Name/TAO");
+
+		try {
+			await pathospeta.transferFrom(nameId1, nameId2, 10000, { from: whitelistedAddress });
+			canTransferFrom = true;
+		} catch (e) {
+			canTransferFrom = false;
+		}
+		assert.equal(canTransferFrom, false, "Whitelisted address can transfer token more than owned balance");
+
+		try {
+			await pathospeta.transferFrom(nameId1, nameId2, 10, { from: whitelistedAddress });
+			canTransferFrom = true;
+		} catch (e) {
+			canTransferFrom = false;
+		}
+		assert.equal(canTransferFrom, true, "Whitelisted address can't transfer token from Name to Name");
+
+		var nameId1Balance = await pathospeta.balanceOf(nameId1);
+		assert.equal(nameId1Balance.toNumber(), 990, "Name has incorrect pathospeta balance");
+		var nameId2Balance = await pathospeta.balanceOf(nameId2);
+		assert.equal(nameId2Balance.toNumber(), 10, "Name has incorrect pathospeta balance");
+
+		try {
+			await pathospeta.transferFrom(nameId1, taoId2, 10, { from: whitelistedAddress });
+			canTransferFrom = true;
+		} catch (e) {
+			canTransferFrom = false;
+		}
+		assert.equal(canTransferFrom, true, "Whitelisted address can't transfer token from Name to TAO");
+
+		var nameId1Balance = await pathospeta.balanceOf(nameId1);
+		assert.equal(nameId1Balance.toNumber(), 980, "Name has incorrect pathospeta balance");
+		var taoId2Balance = await pathospeta.balanceOf(taoId2);
+		assert.equal(taoId2Balance.toNumber(), 10, "TAO has incorrect pathospeta balance");
+
+		try {
+			await pathospeta.transferFrom(taoId1, nameId2, 10, { from: whitelistedAddress });
+			canTransferFrom = true;
+		} catch (e) {
+			canTransferFrom = false;
+		}
+		assert.equal(canTransferFrom, true, "Whitelisted address can't transfer token from TAO to Name");
+
+		var taoId1Balance = await pathospeta.balanceOf(taoId1);
+		assert.equal(taoId1Balance.toNumber(), 990, "TAO has incorrect pathospeta balance");
+		var nameId2Balance = await pathospeta.balanceOf(nameId2);
+		assert.equal(nameId2Balance.toNumber(), 20, "Name has incorrect pathospeta balance");
+
+		try {
+			await pathospeta.transferFrom(taoId1, taoId2, 10, { from: whitelistedAddress });
+			canTransferFrom = true;
+		} catch (e) {
+			canTransferFrom = false;
+		}
+		assert.equal(canTransferFrom, true, "Whitelisted address can't transfer token from TAO to TAO");
+
+		var taoId1Balance = await pathospeta.balanceOf(taoId1);
+		assert.equal(taoId1Balance.toNumber(), 980, "TAO has incorrect pathospeta balance");
+		var taoId2Balance = await pathospeta.balanceOf(taoId2);
+		assert.equal(taoId2Balance.toNumber(), 20, "TAO has incorrect pathospeta balance");
+	});
+
+	it("Whitelisted Address - should be able to burn token from a TAO/Name", async function() {
+		var canWhitelistBurnFrom;
+		try {
+			await pathospeta.whitelistBurnFrom(nameId1, 50, { from: someAddress });
+			canWhitelistBurnFrom = true;
+		} catch (e) {
+			canWhitelistBurnFrom = false;
+		}
+		assert.equal(canWhitelistBurnFrom, false, "Non-whitelisted address can burn token");
+
+		try {
+			await pathospeta.whitelistBurnFrom(someAddress, 50, { from: whitelistedAddress });
+			canWhitelistBurnFrom = true;
+		} catch (e) {
+			canWhitelistBurnFrom = false;
+		}
+		assert.equal(canWhitelistBurnFrom, false, "Whitelisted address can burn token from non Name/TAO");
+
+		try {
+			await pathospeta.whitelistBurnFrom(nameId1, 1000, { from: whitelistedAddress });
+			canWhitelistBurnFrom = true;
+		} catch (e) {
+			canWhitelistBurnFrom = false;
+		}
+		assert.equal(canWhitelistBurnFrom, false, "Whitelisted address can burn token more than owned balance");
+
+		try {
+			await pathospeta.whitelistBurnFrom(nameId1, 50, { from: whitelistedAddress });
+			canWhitelistBurnFrom = true;
+		} catch (e) {
+			canWhitelistBurnFrom = false;
+		}
+		assert.equal(canWhitelistBurnFrom, true, "Whitelisted address can't burn token from Name");
+
+		var nameBalance = await pathospeta.balanceOf(nameId1);
+		assert.equal(nameBalance.toNumber(), 930, "Name has incorrect pathospeta balance");
+
+		try {
+			await pathospeta.whitelistBurnFrom(taoId1, 50, { from: whitelistedAddress });
+			canWhitelistBurnFrom = true;
+		} catch (e) {
+			canWhitelistBurnFrom = false;
+		}
+		assert.equal(canWhitelistBurnFrom, true, "Whitelisted address can't burn token from TAO");
+
+		var taoBalance = await pathospeta.balanceOf(nameId1);
+		assert.equal(taoBalance.toNumber(), 930, "TAO has incorrect pathospeta balance");
+	});
+});
