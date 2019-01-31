65fc98dcd3f524ba3539158a5073853781682033
Johan <johan@paramation.com>
2019-01-31 21:21:45 +0700

    Fix whitelistBurnFrom in TAOCurrency

--- contracts/TAOCurrency.sol
+++ contracts/TAOCurrency.sol
@@ -129,7 +129,7 @@ contract TAOCurrency is TheAO {
 	 * @param _from the address of the sender
 	 * @param _value the amount of money to burn
 	 */
-	function whitelistBurnFrom(address _from, uint256 _value) public inWhitelist returns (bool success) {
+	function whitelistBurnFrom(address _from, uint256 _value) public inWhitelist isNameOrTAO(_from) returns (bool success) {
 		require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
 		balanceOf[_from] = balanceOf[_from].sub(_value);    // Subtract from the targeted balance
 		totalSupply = totalSupply.sub(_value);              // Update totalSupply
